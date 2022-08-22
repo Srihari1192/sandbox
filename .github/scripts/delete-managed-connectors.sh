f60131937fd4803e7e6eb8cfbb1b1f85a21e463d
svenkata <svenkata@redhat.com>
2022-08-22 23:02:34 +0530

    test gh action

--- .github/scripts/delete-managed-connectors.sh
+++ .github/scripts/delete-managed-connectors.sh
@@ -6,9 +6,10 @@
 # This requires the ocm command: https://github.com/openshift-online/ocm-cli
 # You will need to login with `ocm login --token [...]`.
 
-. $(dirname "${BASH_SOURCE[0]}")/../dev/bin/configure.sh
+. dev/bin/configure.sh
 
 export COS_BASE_PATH=${MANAGED_CONNECTORS_CONTROL_PLANE_URL}/api/connector_mgmt/v1
+echo $COS_BASE_PATH
 export CONNECTORS_BASE=${COS_BASE_PATH}/kafka_connectors
 
 MANAGED_CONNECTORS_IDS=$(curl -L --insecure --oauth2-bearer "$(ocm token)" "${CONNECTORS_BASE}" | jq -r '(now - 12*60*60 | strflocaltime("%Y-%m-%dT%H:%M:%S")) as $older  | .items[] | select(.name | startswith("ci-")) | select(.owner="openbridge_kafka_supporting") | select((.created_at | sub("-(?<d>[0-9]-)";"-0\(.d)")) <= $older) | .id')
